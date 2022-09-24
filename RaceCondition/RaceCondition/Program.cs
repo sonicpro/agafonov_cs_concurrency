20e16a59e843c992484eef418007bd96b59ff16a
Volodymyr Tsurko <volodymyr.tsurko@qognify.com>
2022-09-24 17:38:19 +0300

    Added race condition.

--- RaceCondition/RaceCondition/Program.cs
+++ RaceCondition/RaceCondition/Program.cs
@@ -1,15 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace RaceCondition
 {
     class Program
     {
-        static void Main(string[] args)
+        const int iterations = 10000;
+
+        static void Main()
         {
+            var result = ProcessData();
+            Console.WriteLine(result);
+        }
+
+        private static int ProcessData()
+        {
+            var counter = 0;
+            ThreadStart proc = () =>
+            {
+                for (int i = 0; i < iterations; i++)
+                {
+                    counter++;
+                    Thread.SpinWait(10);
+                    counter--;
+                }
+            };
+            var tasks = Enumerable.Range(1, 4)
+                .Select(_ => new Thread(proc))
+                .ToArray();
+            foreach (Thread thread in tasks)
+            {
+                thread.Start();
+            }
+            
+            foreach (Thread thread in tasks)
+            {
+                thread.Join();
+            }
+
+            return counter;
         }
     }
 }
+
